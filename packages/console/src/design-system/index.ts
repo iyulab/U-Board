bc74f800e6875e19eddd01c964729650a1688b71
zizixmas <zizixmas@gmail.com>
2026-08-28 18:59:46 +0900

    feat(console): add design-system barrel export

---
+++ packages/console/src/design-system/index.ts
@@ -0,0 +1,7 @@
+export { Button } from './Button.js';
+export { Alert } from './Alert.js';
+export { Badge } from './Badge.js';
+export { FormField } from './FormField.js';
+export { Modal } from './Modal.js';
+export { AppShell } from './AppShell.js';
+export { ToastProvider, useToast } from './Toast.js';
